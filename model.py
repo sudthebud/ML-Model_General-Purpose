fb2d7b7a915f5b7b03a4e4f38eb2588bc8bd462e
sudthebud <sudharshan.srinivasan.1@gmail.com>
2025-09-26 23:36:38 -0400

    Feed forward function

--- model.py
+++ model.py
@@ -36,6 +36,35 @@ class Model:
 
 
     # Run the data through the layers of the neural network,
-    # and return the output.
-    def feed_forward(input):
-        
+    # and return the output. Input can be more than 1 column,
+    # which allows for vectorization of the input data (enabling
+    # fast processing of the input and output using matrix math 
+    # rather than looping.
+    def feed_forward(self, input):
+        for i in range(len(self.numHiddenLayerNodes) + 1): # For each layer in the neural network...
+            
+            # Multiply the weights for that layer by the input
+            # (which is either the input for the model or the)
+            # previous layer. For each node in the layer, we 
+            # multiply all the nodes in the previous layer
+            # with the weight going into the node in the current
+            # layer, then add them together. We do it in the order
+            # of "weights * input" so that the result comes out
+            # to the number of nodes in the current layer (so if
+            # the input is 2 x 1 and the number of nodes in the
+            # current layer is 3, then the number of weights is
+            # 3 (for current layer nodes) x 2 (for previous layer
+            # nodes), and the result is a 3 x 1 matrix.
+            currLayer = self.weights[i] @ input
+
+            # Add the biases of each node in the layer. Numpy will
+            # automatically broadcast the bias matrix if the number
+            # of columns in the current layer is greater than 1.
+            currLayer += self.biases[i]
+
+            # Apply the activation function to all nodes in the 
+            # current layer.
+            currLayer = model_setup.activation(currLayer, self.activationFunc if isinstance(self.activationFunc, list[int]) else self.activationFunc[i])
+
+            if i == len(self.numHiddenLayerNodes) + 1: return currLayer
+            input = currLayer
